df2a2a5d1789317146d3bcb429aa830b7ba09c39
Choochoth <choochoth@gmail.com>
2025-09-16 13:16:54 +0700

    fixed

--- src/player.ts
+++ src/player.ts
@@ -8,16 +8,16 @@ const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["nus9331", "manus9331", "areeroon"],
     high:  ["nus9331", "manus9331", "areeroon"],
-    mid: ["nus9331", "manus9331", "areeroon", "Preechar", "VIP0955171905", "poypy789"],
-    low: ["nus9331", "manus9331", "areeroon", "Preechar", "VIP0955171905", "poypy789"],
-    all: ["nus9331", "manus9331", "Preechar", "kootong", "areeroon", "VIP0955171905", "poypy789"]
+    mid: ["nus9331", "manus9331", "areeroon", "VIP0955171905", "poypy789"],
+    low: ["nus9331", "manus9331", "areeroon", "VIP0955171905", "poypy789"],
+    all: ["nus9331", "manus9331", "kootong", "areeroon", "VIP0955171905", "poypy789"]
   },
   thai_jun88k36: {
-    very_high:["nus9331", "manus9331", "aroon11", "jajloy228"],
-    high: ["aroon11", "manus9331", "nus9331", "jajloy228"],
-    mid: ["manoon9331", "jajloy228", "aroon11"],
+    very_high:["nus9331", "manus9331", "aroon11"],
+    high: ["aroon11", "manus9331", "nus9331"],
+    mid: ["manoon9331", "aroon11"],
     low: [],
-    all: ["nus9331", "manoon9331", "manus9331", "jajloy228", "aroon11"],
+    all: ["nus9331", "manoon9331", "manus9331", "aroon11"],
   }
 };
 
