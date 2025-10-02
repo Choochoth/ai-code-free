d836ea1d011f95f6737a5a71bf558a8cadbbd682
Choochoth <choochoth@gmail.com>
2025-10-02 12:46:54 +0700

    add

--- src/main.ts
+++ src/main.ts
@@ -1013,9 +1013,9 @@ async function initializeService() {
 // 🚀 startProCodeLoop (รองรับ abort)
 async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
-    minPoint = 20;
+    minPoint = 18;
   } else {
-    minPoint = 16;
+    minPoint = 18;
   }
 
   const siteQueue = siteQueues[siteName];
