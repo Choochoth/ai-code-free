eb18bc85f4df1d92a7302633f7ea85a5bc0e9efb
Choochoth <choochoth@gmail.com>
2025-10-29 03:16:37 +0700

    minPoint Update

--- src/main.ts
+++ src/main.ts
@@ -540,9 +540,9 @@ async function initializeService() {
 // 🚀 startProCodeLoop (รองรับ abort)
 async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
-    minPoint = 12;
+    minPoint = 15;
   } else {
-    minPoint = 12;
+    minPoint = 13;
   }
 
   const siteQueue = siteQueues[siteName];
