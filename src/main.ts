b93f5d637e0ec2e2f95abb45c8e7208c08627026
Choochoth <choochoth@gmail.com>
2025-10-29 11:25:01 +0700

    update jun88

--- src/main.ts
+++ src/main.ts
@@ -540,9 +540,9 @@ async function initializeService() {
 // 🚀 startProCodeLoop (รองรับ abort)
 async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
-    minPoint = 13;
+    minPoint = 15;
   } else {
-    minPoint = 13;
+    minPoint = 15;
   }
 
   const siteQueue = siteQueues[siteName];
