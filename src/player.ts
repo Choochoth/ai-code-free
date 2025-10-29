3b014777e7bd4471eeae4179a32b8ed038d9fed5
Choochoth <choochoth@gmail.com>
2025-10-29 17:50:11 +0700

    add

--- src/player.ts
+++ src/player.ts
@@ -15,9 +15,9 @@ import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 //   thai_jun88k36: {
 //     very_high:["manus9331", "aroon11", "nus9331"],
 //     high: ["aroon11", "nus9331", "manus9331"],
-//     mid: ["aroon11", "bank0760", "ary11", "ammies", "wuttichaizx77"],
-//     low: ["bank0760", "ary11", "ammies", "wuttichaizx77"],
-//     all: ["bank0760", "ary11", "ammies", "wuttichaizx77"]
+//     mid: ["aroon11", "bank0760", "ary11", "wuttichaizx77"],
+//     low: ["bank0760", "ary11", "wuttichaizx77"],
+//     all: ["bank0760", "ary11", "wuttichaizx77"]
 //   }
 // };
 
@@ -25,16 +25,16 @@ const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["nus9331", "poypy789", "manus9331", "VIP0955171905"],
     high:  ["manus9331", "VIP0955171905", "nus9331", "poypy789"],
-    mid: ["manus9331", "VIP0955171905", "nus9331", "poypy789"],
-    low: ["VIP0955171905", "kootong", "poypy789"],
-    all: ["nus9331", "manus9331", "VIP0955171905", "poypy789", "kootong", "areeroon"]
+    mid: ["manus9331", "VIP0955171905", "nus9331", "poypy789", "jojo159"],
+    low: ["VIP0955171905", "kootong", "poypy789", "jojo159"],
+    all: ["nus9331", "manus9331", "VIP0955171905", "poypy789", "kootong", "jojo159"]
   },
   thai_jun88k36: {
     very_high:["manus9331", "aroon11", "nus9331"],
     high: ["aroon11", "nus9331", "manus9331"],
-    mid: ["aroon11", "bank0760", "ary11", "ammies", "wuttichaizx77"],
-    low: ["bank0760", "ary11", "ammies", "wuttichaizx77"],
-    all: ["aroon11", "nus9331", "manus9331", "bank0760", "ary11", "ammies", "wuttichaizx77"]
+    mid: ["aroon11", "bank0760", "ary11", "wuttichaizx77"],
+    low: ["bank0760", "ary11", "wuttichaizx77"],
+    all: ["aroon11", "nus9331", "manus9331", "bank0760", "ary11", "wuttichaizx77"]
   }
 };
 
