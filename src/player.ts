bb5f2e147ab7e1c5ce59573ca17b8f76916728ea
codeaiokvip <codeaiokvip@gmail.com>
2025-11-03 19:06:18 +0700

    Update player.ts
    Add user player

--- src/player.ts
+++ src/player.ts
@@ -14,11 +14,11 @@ const playerPools: Record<string, PlayerPool> = {
     all: ["manus9331", "VIP0955171905", "nus9331", "CHEKKKK16", "poypy789", "kootong", "tong551212"]
   },
   thai_jun88k36: {
-    very_high:["manus9331", "aroon11", "nus9331", "CHEKKKK14"],
-    high: ["aroon11", "nus9331", "manus9331", "CHEKKKK14"],
-    mid: ["aroon11", "bank0760", "ary11", "nuschai", "CHEKKKK14", "nus9331"],
+    very_high:["manus9331", "aroon11", "nus9331", "CHEKKKK16", "CHEKKKK19"],
+    high: ["aroon11", "nus9331", "manus9331", "CHEKKKK16", "CHEKKKK19"],
+    mid: ["aroon11", "bank0760", "ary11", "nuschai",  "CHEKKKK16", "CHEKKKK19", "nus9331"],
     low: ["bank0760", "ary11", "nuschai"],
-    all: ["bank0760", "ary11", "manus9331", "aroon11", "nus9331", "nuschai", "CHEKKKK14"]
+    all: ["bank0760", "ary11", "manus9331", "aroon11", "nus9331", "nuschai", "CHEKKKK16", "CHEKKKK19"]
   }
 };
 
