b546f7afc363f511afebb6af09254b2c88058b5c
Choochoth <choochoth@gmail.com>
2025-11-04 11:01:06 +0700

    update

--- src/playerTelegram.ts
+++ src/playerTelegram.ts
@@ -4,11 +4,15 @@
       users: [
         {
           TelegramId: 7795985512,
-          users: ["ammies", "natties"]
+          users: ["AMMIES", "natties"]
         },
         {
           TelegramId: 8371851714,
           users: ["wuttichaizx77"]
+        },
+        {
+          TelegramId: 1891499609,
+          users: ["Chekkkk04", "Chekkkk09", "Chekkkk14", "Chekkkk18"]
         }
       ]
     }
