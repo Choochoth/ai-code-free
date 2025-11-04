d1ac52bf65b92deb9b40288dc1c6b9ab7cd64578
Choochoth <choochoth@gmail.com>
2025-11-04 23:15:48 +0700

    ok

--- src/playerTelegram.ts
+++ src/playerTelegram.ts
@@ -12,7 +12,7 @@
         },
         {
           TelegramId: 1891499609,
-          users: ["Chekkkk04", "Chekkkk09", "Chekkkk12", "Chekkkk16", "Chekkkk18", "Chekkkk19"]
+          users: ["Chekkkk04", "Chekkkk09", "Chekkkk12", "Chekkkk16", "Chekkkk18", "Chekkkk19", "Chekkkk14", "Chekkkk04", "Chekkkk08", "notea0035", "manu69", "AMJA1122", "momay199314", "Darin668"]
         }
       ]
     }
