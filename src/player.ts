7abe22bbf8db0068abe32c85a3c3442fa950247f
Choochoth <choochoth@gmail.com>
2025-11-06 22:39:12 +0700

    okay

--- src/player.ts
+++ src/player.ts
@@ -4,8 +4,6 @@ import path from "path";
 import { AppliedPlayer, ApplyCodeToday, PlayerPool, PlayerLock } from "./types/player";
 import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 
-
-
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["manus9331", "nus9331", "poypy789", "VIP0955171905"],
