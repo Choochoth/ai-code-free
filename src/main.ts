63240d03bc2ff147186e8302e042cbbdc7cc6d9c
Choochoth <choochoth@gmail.com>
2025-11-08 13:24:27 +0700

    ok update

--- src/main.ts
+++ src/main.ts
@@ -543,7 +543,7 @@ async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
     minPoint = 18;
   } else {
-    minPoint = 18;
+    minPoint = 15;
   }
 
   const siteQueue = siteQueues[siteName];
