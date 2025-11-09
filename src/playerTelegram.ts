3ab9314316c2e868de1cbf1cbe0d1cc9b0a703d9
Choochoth <choochoth@gmail.com>
2025-11-10 00:23:42 +0700

    update

--- src/playerTelegram.ts
+++ src/playerTelegram.ts
@@ -4,7 +4,7 @@
       users: [
         {
           TelegramId: 7795985512,
-          users: ["AMMIES", "natties"]
+          users: ["Ammies", "natties"]
         },
         {
           TelegramId: 8371851714,
@@ -12,7 +12,7 @@
         },
         {
           TelegramId: 1891499609,
-          users: ["Chekkkk04", "Chekkkk09", "Chekkkk12", "Chekkkk16", "Chekkkk18", "Chekkkk19", "Chekkkk14", "Chekkkk04", "Chekkkk08", "notea0035", "manu69", "AMJA1122", "momay199314", "Darin668", "chekkkk18", "tong1689"]
+          users: ["Chekkkk04", "Chekkkk09", "Chekkkk12", "Chekkkk16", "Chekkkk18", "Chekkkk19", "Chekkkk14","Chekkkk08", "notea0035", "manu69", "AMJA1122", "momay199314", "Darin668", "tong1689"]
         },
         {
           TelegramId: 7777347424,
@@ -24,6 +24,7 @@
 
 
   const freeUsers:any = [];
+  // const rewardUsers:any = ["Ammies", "Chekkkk16", "Chekkkk18", "Chekkkk19", "Chekkkk14"];
   const rewardUsers:any = [];
 
   export {playerTelegram, freeUsers, rewardUsers}
