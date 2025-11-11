f38435c9d099f8aeb7bd67e87927c887e495a211
Choochoth <choochoth@gmail.com>
2025-11-11 16:49:08 +0700

    update

--- src/playerTelegram.ts
+++ src/playerTelegram.ts
@@ -4,7 +4,7 @@
       users: [
         {
           TelegramId: 7795985512,
-          users: ["Ammies", "natties"]
+          users: ["amies", "natties"]
         },
         {
           TelegramId: 8371851714,
@@ -12,7 +12,7 @@
         },
         {
           TelegramId: 1891499609,
-          users: ["Chekkkk04", "Chekkkk09", "Chekkkk12", "Chekkkk16", "Chekkkk18", "Chekkkk19", "Chekkkk14", "Chekkkk04", "Chekkkk08", "notea0035", "manu69", "AMJA1122", "momay199314", "Darin668", "chekkkk18", "tong1689"]
+          users: ["chekkkk04", "chekkkk09", "chekkkk12", "chekkkk16", "chekkkk18", "chekkkk19", "chekkkk14", "chekkkk04", "chekkkk08"]
         },
         {
           TelegramId: 7777347424,
@@ -20,13 +20,14 @@
         },
         {
           TelegramId: 788225380,
-          users: ["Apoung"]
-        }        
+          users: ["apoung"]
+        }          
       ]
     }
   ];
 
 
   const freeUsers:any = [];
-const rewardUsers:any = ["ary11", "bank7060", "nuschai", "chekkkk16", "chekkkk18", "chekkkk19", "chekkkk14", "apoung", "ammies", "chekkkk15", "poypy789", "vip0955171905"];
+  const rewardUsers:any = ["ary11", "bank7060", "nuschai", "chekkkk16", "chekkkk18", "chekkkk19", "chekkkk14", "apoung", "ammies", "chekkkk15", "poypy789", "vip0955171905"];
   export {playerTelegram, freeUsers, rewardUsers}
+  
