54aa3e109e30ab82891edff6198a9d9b55a24486
Choochoth <choochoth@gmail.com>
2025-11-16 08:28:17 +0700

    add api

--- src/public/js/package.js
+++ src/public/js/package.js
@@ -1,7 +1,7 @@
 let packages = [];
 let selectedPackage = null;
 let selectedSite = "jun88";
-const apiBase = "https://ai-code-api-production-2fd6.up.railway.app";
+const apiBase = "https://okvip-api-production.up.railway.app";
 
 // ================== โหลดข้อมูลแพ็กเกจจาก API ==================
 async function loadPackages() {
