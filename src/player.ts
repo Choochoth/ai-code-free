570865b7f4b79e829ff8a26fb62ef199246aac8d
Choochoth <choochoth@gmail.com>
2025-11-21 22:41:56 +0700

    iammy89

--- src/player.ts
+++ src/player.ts
@@ -8,9 +8,9 @@ const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["manus9331", "nus9331", "sjd9331"],
     high:  ["manus9331", "nus9331", "vip0955171905", "poypy789", "sjd9331"],
-    mid: ["manus9331", "nus9331", "vip0955171905", "poypy789", "sjd9331", "Iammy89"],
-    low: ["vip0955171905", "poypy789", "sjd9331", "Iammy89"],
-    all: ["vip0955171905", "poypy789", "Iammy89"]
+    mid: ["manus9331", "nus9331", "vip0955171905", "poypy789", "sjd9331", "iammy89"],
+    low: ["vip0955171905", "poypy789", "sjd9331", "iammy89"],
+    all: ["vip0955171905", "poypy789", "iammy89"]
   },
   thai_jun88k36: {
     very_high:["manus9331", "aroon11", "nus9331", "sjd9331", "nus3206", "iamkanya"],
