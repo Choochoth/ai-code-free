4078ca3536b17a1bf0e31a72bc5d2e3d2c06e833
codeaiokvip <codeaiokvip@gmail.com>
2025-12-19 02:04:00 +0700

    Update player.ts

--- src/player.ts
+++ src/player.ts
@@ -9,13 +9,13 @@ import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["manus9331", "nus9331", "aroon11"],
-    high:  ["manus9331", "nus9331", "aroon11", "IAMMY89", "PP199737"],
+    high:  ["manus9331", "nus9331", "aroon11"],
     mid: ["manus9331", "nus9331", "vip0955171905", "poypy789", "aroon11", "IAMMY89", "PP199737"],
     low: ["vip0955171905", "poypy789", "borvon", "kootong"],
     all: ["manus9331", "poypy789", "aroon11", "VIP0955171905", "nus9331", "IAMMY89", "PP199737"]
   },
   thai_jun88k36: {
-    very_high:["aroon11", "nus9331", "manus9331", "nuschai", "IAMKANYA"],
+    very_high:["aroon11", "nus9331", "manus9331", "nuschai"],
     high: ["manus9331", "nus9331", "aroon11", "nuschai", "IAMKANYA"],
     mid: ["ary11", "IAMKANYA", "YALWYALW"],
     low: ["ary11"],
