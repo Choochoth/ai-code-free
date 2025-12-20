af5a40c71c94bdf4701988b705e3c994151a920d
Choochoth <choochoth@gmail.com>
2025-12-20 15:17:12 +0700

    ok

--- src/player.ts
+++ src/player.ts
@@ -7,13 +7,13 @@ import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["manus9331", "nus9331", "aroon11", "IAMMY89"],
-    high:  ["manus9331", "nus9331", "aroon11", "IAMMY89", "vip0955171905"],
+    high:  ["manus9331", "nus9331", "aroon11", "IAMMY89", "poypy789"],
     mid: ["manus9331", "nus9331", "vip0955171905", "poypy789", "aroon11", "IAMMY89", "YALW1994"],
     low: ["vip0955171905", "poypy789", "borvon", "kootong"],
     all: ["manus9331", "poypy789", "aroon11", "VIP0955171905", "nus9331", "IAMMY89", "YALW1994"]
   },
   thai_jun88k36: {
-    very_high:["aroon11", "nus9331", "manus9331", "nuschai", "IAMKANYA"],
+    very_high:["aroon11", "nus9331", "manus9331", "nuschai"],
     high: ["manus9331", "nus9331", "aroon11", "nuschai", "IAMKANYA"],
     mid: ["ary11", "IAMKANYA", "junplayer", "bank0760"],
     low: ["ary11"],
