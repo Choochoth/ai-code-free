4bf53e689105d60b49b1096b1c844a0cbaef924c
Choochoth <choochoth@gmail.com>
2025-12-20 19:44:42 +0700

    ok

--- src/main.ts
+++ src/main.ts
@@ -542,9 +542,9 @@ async function initializeService() {
 // 🚀 startProCodeLoop (รองรับ abort)
 async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
-    minPoint = 20;
+    minPoint = 18;
   } else {
-    minPoint = 15;
+    minPoint = 13;
   }
 
   const siteQueue = siteQueues[siteName];
--- src/player.ts
+++ src/player.ts
@@ -5,14 +5,13 @@ import { AppliedPlayer, ApplyCodeToday, PlayerPool, PlayerLock } from "./types/p
 import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 
 
-
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["manus9331", "nus9331", "aroon11", "IAMMY89"],
-    high:  ["manus9331", "nus9331", "aroon11", "IAMMY89", "poypy789"],
+    high:  ["manus9331", "nus9331", "aroon11", "IAMMY89", "YALW1994", "poypy789"],
     mid: ["manus9331", "nus9331", "vip0955171905", "poypy789", "aroon11", "IAMMY89", "YALW1994"],
     low: ["vip0955171905", "poypy789", "borvon", "kootong"],
-    all: ["manus9331", "poypy789", "aroon11", "VIP0955171905", "nus9331", "IAMMY89", "YALW1994"]
+    all: ["manus9331", "poypy789", "aroon11", "VIP0955171905", "nus9331"]
   },
   thai_jun88k36: {
     very_high:["aroon11", "nus9331", "manus9331", "nuschai"],
