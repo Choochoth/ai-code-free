1de3dadfdeb25f64ab2496454049ffeef25dbeaf
Choochoth <choochoth@gmail.com>
2026-01-19 00:11:29 +0700

    ok

--- src/player.ts
+++ src/player.ts
@@ -14,13 +14,15 @@ const playerPools: Record<string, PlayerPool> = {
     all: ["manus9331", "poypy789", "aroon11", "vip0955171905", "nus9331"]
   },
   thai_jun88k36: {
-    very_high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760", "iamkanya", "tunpayong", "Tunpayongdnk"],
-    high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760", "iamkanya", "tunpayong", "Tunpayongdnk"],
-    mid:  ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760", "iamkanya", "tunpayong", "Tunpayongdnk"],
+    very_high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"],
+    high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"],
+    mid:  ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"],
     low: ["aroon11", "nuschai", "ary11", "bank0760"],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760", "iamkanya", "tunpayong", "Tunpayongdnk"]
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"]
   }
 };
+
+
 
 
 export type Site = keyof typeof playerPools;
