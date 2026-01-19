bfb091c41e64592fcf197d5b1e7e3f02e60125a9
Choochoth <choochoth@gmail.com>
2026-01-19 09:44:21 +0700

    ok

--- src/main.ts
+++ src/main.ts
@@ -131,7 +131,7 @@ let minPoint: number = 8;
 
 const POLL_TARGETS: PollTarget[] = (() => {
   const t = loadPollTargetsFromEnv();
-  return t.length ? t : [{"channelId":"-1002668963498","messageId":3029},{"channelId":"-1002142874457","messageId":5028},{"channelId":"-1002519263985","messageId":3963}];
+  return t.length ? t : [{"channelId":"-1002142874457","messageId":5035},{"channelId":"-1002668963498","messageId":3035},{"channelId":"-1002519263985","messageId":3968}];
 })();
 
 function stopPolling() {
