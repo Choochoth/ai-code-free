eea366be7ebb9d2c8e779a38669f0e98e1d9753a
Choochoth <choochoth@gmail.com>
2026-01-23 21:47:33 +0700

    S1-T

--- src/services/promoCodeApi.ts
+++ src/services/promoCodeApi.ts
@@ -264,7 +264,7 @@ export async function jun88PollTarget() {
 
   try {
     const response = await limitedGet(url, {
-      timeout: 1500, // 🔥 ไม่ต้อง 15s
+      timeout: 2000, // 🔥 ไม่ต้อง 15s
     });
 
     console.log("✅ poll-targets:", response.data);
