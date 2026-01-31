00b6858c614f200726f1339f3c679507d8784892
Choochoth <choochoth@gmail.com>
2026-01-31 13:29:15 +0700

    ok

--- src/player.ts
+++ src/player.ts
@@ -6,21 +6,20 @@ import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
-    very_high: ["manus9331", "nus9331", "aroon11"],
-    high:  ["manus9331", "nus9331", "aroon11"],
-    mid: ["manus9331", "nus9331", "aroon11", "poypy789", "vip0955171905"],
+    very_high: ["manus9331", "nus9331", "aroon11", "iammy89"],
+    high:  ["manus9331", "nus9331", "aroon11", "chekkkk16", "iammy89"],
+    mid: ["manus9331", "nus9331", "aroon11", "poypy789", "chekkkk16", "iammy89"],
     low: ["vip0955171905", "poypy789", "areeroon"],
-    all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11"]
+    all: ["vip0955171905", "poypy789", "chekkkk16", "manus9331", "nus9331", "aroon11", "iammy89"]
   },
   thai_jun88k36: {
     very_high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"],
-    high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"],
-    mid:  ["manus9331", "aroon11"],
+    high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760", "iamkanya", "frankk2709", "lucifer101", "NAMCHOK200"],
+    mid:  ["manus9331", "aroon11", "iamkanya", "frankk2709", "lucifer101", "NAMCHOK200"],
     low: [],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"]
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760", "iamkanya", "frankk2709", "lucifer101", "NAMCHOK200"]
   }
 };
-
 
 export type Site = keyof typeof playerPools;
 
