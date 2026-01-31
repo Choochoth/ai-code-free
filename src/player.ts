cdf81104358514c57a249d9629de1090231b3563
Choochoth <choochoth@gmail.com>
2026-01-31 18:34:03 +0700

    S2-DP

--- src/player.ts
+++ src/player.ts
@@ -7,10 +7,10 @@ import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["manus9331", "nus9331", "aroon11", "iammy89"],
-    high:  ["manus9331", "nus9331", "aroon11", "chekkkk16", "iammy89"],
-    mid: ["manus9331", "nus9331", "aroon11", "poypy789", "chekkkk16", "iammy89"],
+    high:  ["manus9331", "nus9331", "aroon11", "iammy89"],
+    mid: ["manus9331", "nus9331", "aroon11", "poypy789", "iammy89"],
     low: ["vip0955171905", "poypy789", "areeroon"],
-    all: ["vip0955171905", "poypy789", "chekkkk16", "manus9331", "nus9331", "aroon11", "iammy89"]
+    all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11", "iammy89"]
   },
   thai_jun88k36: {
     very_high: ["manus9331", "nus9331", "aroon11", "nuschai", "ary11", "bank0760"],
