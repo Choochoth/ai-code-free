75a68a607a5185155443a35a43db31fee04843c2
Choochoth <choochoth@gmail.com>
2026-03-20 22:15:42 +0700

    ok-deploy=ss3

--- src/main.ts
+++ src/main.ts
@@ -799,9 +799,9 @@ async function initializeService() {
 // 🚀 startProCodeLoop (รองรับ abort)
 async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
-    minPoint = 20;
+    minPoint = 17;
   } else {
-    minPoint = 20;
+    minPoint = 17;
   }
 
   const siteQueue = siteQueues[siteName];
