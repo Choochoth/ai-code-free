8d77b7b1a6427e0cd090ae695bd17788fa5c794d
Choochoth <choochoth@gmail.com>
2026-03-20 22:05:00 +0700

    Dpl-S1

--- src/main.ts
+++ src/main.ts
@@ -801,7 +801,7 @@ async function initializeService() {
 // 🚀 startProCodeLoop (รองรับ abort)
 async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
-    minPoint = 20;
+    minPoint = 17;
   } else {
     minPoint = 20;
   }
