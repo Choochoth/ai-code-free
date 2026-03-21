48191e5cc338733154ca199d549cf29abac6333a
Choochoth <choochoth@gmail.com>
2026-03-21 15:22:45 +0700

    chat-ss3

--- src/main.ts
+++ src/main.ts
@@ -731,7 +731,6 @@ async function initializeService() {
           "-1002668963498",
           "-1002142874457",
           "-1002040396559",
-          "-1002544749433",
         ]);
 
         client.addEventHandler(
