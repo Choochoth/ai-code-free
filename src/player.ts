96784340b5cf7e063e214a106276a92fd6d766bc
okcodeai-s03 <okcodeai3@gmail.com>
2026-03-22 15:09:23 +0700

    Update player.ts

--- src/player.ts
+++ src/player.ts
@@ -6,11 +6,11 @@ import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
-    very_high: ["aroon11", "nus9331", "Shwgf12"],
-    high:  ["nus9331", "aroon11", "poypy789", "manus9331", "vip0955171905", "Shwgf12"],
+    very_high: ["aroon11", "nus9331", "assss1"],
+    high:  ["nus9331", "aroon11", "poypy789", "manus9331", "vip0955171905", "assss1"],
     mid: ["manus9331", "aroon11", "poypy789", "vip0955171905"],
     low: ["vip0955171905", "poypy789", "areeroon"],
-    all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11", "Shwgf12"]
+    all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11", "assss1"]
   },
   thai_jun88k36: {
     very_high: ["nuschai", "ary11", "bank0760", "nus9331", "aroon11"],
