73314e5b535d4ad3d10fc1048a852a222975a851
okcodeai-s03 <okcodeai3@gmail.com>
2026-03-27 00:33:22 +0700

    Update player.ts

--- src/player.ts
+++ src/player.ts
@@ -13,11 +13,11 @@ const playerPools: Record<string, PlayerPool> = {
     all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11"]
   },
   thai_jun88k36: {
-    very_high: ["nuschai", "NAMCHOK200", "Namchok20", "Pakkad123456", "nus9331"],
-    high: ["nuschai", "NAMCHOK200", "Namchok20", "Pakkad123456", "nus9331"],
-    mid:  ["nuschai", "NAMCHOK200", "Namchok20", "Pakkad123456", "nus9331"],
+    very_high: ["nuschai", "NAMCHOK200", "Namchok20", "sfwcwu", "nus9331"],
+    high: ["nuschai", "NAMCHOK200", "Namchok20", "sfwcwu", "nus9331"],
+    mid:  ["nuschai", "NAMCHOK200", "Namchok20", "sfwcwu", "nus9331"],
     low: [],
-    all: ["nuschai", "NAMCHOK200", "Namchok20", "Pakkad123456", "nus9331"],
+    all: ["nuschai", "NAMCHOK200", "Namchok20", "sfwcwu", "nus9331"],
   }
 }
 
