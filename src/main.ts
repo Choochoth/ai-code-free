0f923430da5abc4c879557bdf2531d1df4d4b9f9
Choochoth <choochoth@gmail.com>
2026-03-30 00:15:53 +0700

    re-dpl-ss2

--- src/main.ts
+++ src/main.ts
@@ -803,7 +803,7 @@ async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
     minPoint = 15;
   } else {
-    minPoint = 15;
+    minPoint = 20;
   }
 
   const siteQueue = siteQueues[siteName];
