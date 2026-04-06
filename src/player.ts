ae50ea599a9bf62597fcce4aba40612625de2781
Choochoth <choochoth@gmail.com>
2026-04-07 01:53:21 +0700

    ok

--- src/player.ts
+++ src/player.ts
@@ -4,6 +4,22 @@ import path from "path";
 import { AppliedPlayer, ApplyCodeToday, PlayerPool, PlayerLock } from "./types/player";
 import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 
+// const playerPools: Record<string, PlayerPool> = {
+//   thai_789bet: {
+//     very_high: ["aroon11", "nus9331", "vip0955171905", "poypy789"],
+//     high:  ["nus9331", "aroon11", "poypy789", "manus9331", "vip0955171905"],
+//     mid: ["manus9331", "aroon11", "poypy789", "vip0955171905"],
+//     low: ["vip0955171905", "poypy789", "manus9331"],
+//     all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11"]
+//   },
+//   thai_jun88k36: {
+//     very_high: ["nuschai", "ary11", "aroon11", "nus9331", "bank0760"],
+//     high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11"],
+//     mid:  ["manus9331", "nus9331", "ary11", "bank0760", "aroon11", "nuschai"],
+//     low: ["bank0760", "manus9331", "ary11", "aroon11", "nuschai"],
+//     all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11"]
+//   }
+// }
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["aroon11", "nus9331", "vip0955171905", "poypy789"],
@@ -13,14 +29,13 @@ const playerPools: Record<string, PlayerPool> = {
     all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11"]
   },
   thai_jun88k36: {
-    very_high: ["nuschai", "ary11", "aroon11", "nus9331", "bank0760"],
-    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11"],
-    mid:  ["manus9331", "nus9331", "ary11", "bank0760", "aroon11", "nuschai"],
-    low: ["bank0760", "manus9331", "ary11", "aroon11", "nuschai"],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11"]
+    very_high: ["nuschai", "aroon11", "nus9331", "bank0760"],
+    high: ["nus9331", "bank0760", "nuschai", "aroon11"],
+    mid:  ["nus9331", "bank0760", "aroon11", "nuschai"],
+    low: ["bank0760", "manus9331", "aroon11", "nuschai"],
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760"]
   }
 }
-
 export type Site = keyof typeof playerPools;
 
 const baseDir = __dirname;
