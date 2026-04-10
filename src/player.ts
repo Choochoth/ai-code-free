8f91017271bf7bdee970a1548e6e6526ef2419fb
Choochoth <choochoth@gmail.com>
2026-04-11 03:33:40 +0700

    ok-ss2

--- src/player.ts
+++ src/player.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { AppliedPlayer, ApplyCodeToday, PlayerPool, PlayerLock } from "./types/player";
 import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 
+
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
     very_high: ["aroon11", "nus9331", "areeroon"],
@@ -13,14 +14,13 @@ const playerPools: Record<string, PlayerPool> = {
     all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11", "areeroon"]
   },
   thai_jun88k36: {
-    very_high: ["nuschai", "ary11", "aroon11", "nus9331", "bank0760"],
+    very_high: ["nuschai", "ary11", "aroon11", "nus9331", "bank0760", "nujan2536"],
     high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "nujan2536"],
     mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "nujan2536"],
     low: ["bank0760", "manus9331", "ary11", "aroon11", "nuschai"],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "nujan2536"]
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11"]
   }
 }
-
 
 export type Site = keyof typeof playerPools;
 
