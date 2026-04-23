504cb3228d6445d2540d37b1bceb00d39749f669
Choochoth <choochoth@gmail.com>
2026-04-23 16:31:57 +0700

    service-ss3

--- src/player.ts
+++ src/player.ts
@@ -14,11 +14,11 @@ const playerPools: Record<string, PlayerPool> = {
     all: ["vip0955171905", "poypy789", "manus9331", "nus9331", "aroon11", "areeroon"]
   },
   thai_jun88k36: {
-    very_high: ["nuschai", "ary11", "aroon11", "nus9331", "bank0760"],
-    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11"],
-    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai"],
+    very_high: ["nuschai", "ary11", "aroon11", "nus9331", "bank0760", "areeroon"],
+    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "areeroon"],
+    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "areeroon"],
     low: ["bank0760", "manus9331", "ary11", "nus9331", "aroon11", "nuschai"],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11"]
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "areeroon"]
   }
 }
 
