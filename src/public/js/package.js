181c3c0fa533276743d794bff347a68c80fbfd82
Choochoth <choochoth@gmail.com>
2026-05-06 00:44:58 +0700

    ok-s03

--- src/public/js/package.js
+++ src/public/js/package.js
@@ -1,7 +1,7 @@
 let packages = [];
 let selectedPackage = null;
 let selectedSite = "jun88";
-const apiBase = "https://okcode-ai-api-production-0585.up.railway.app";
+const apiBase = "https://okcode-api-s03-production.up.railway.app";
 
 // ================== โหลดข้อมูลแพ็กเกจจาก API ==================
 async function loadPackages() {
