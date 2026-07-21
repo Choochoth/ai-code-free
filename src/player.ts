feac2466eabed49050b7c3fee2aedac04161f75b
Choochoth <choochoth@gmail.com>
2026-07-21 21:50:52 +0700

    ok code dpm

--- src/player.ts
+++ src/player.ts
@@ -10,7 +10,7 @@ const playerPools: Record<string, PlayerPool> = {
     very_high: ["aroon11", "nus9331", "nus3206", "areeroon", "manus9331", "sjd9331"],
     high:  ["nus9331", "aroon11", "sjd9331", "areeroon", "manus9331", "nus3206"],
     mid: ["aroon11", "nus3206", "areeroon", "nus9331", "sjd9331" , "manus9331"],
-    low: ["vip0955171905", "nus3206", "poypy789", "manus9331", "aroon11", "nus9331", "areeroon", "sjd9331"],
+    low: ["nus3206", "manus9331", "aroon11", "nus9331", "areeroon", "sjd9331"],
     all: ["vip0955171905", "poypy789", "nus3206", "manus9331", "nus9331", "aroon11", "areeroon", "sjd9331"]
   },
   thai_jun88k36: {
