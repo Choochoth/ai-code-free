b2a363c79b9b0d4e5f59f2bae8cf50a6bea50133
Choochoth <choochoth@gmail.com>
2026-07-30 16:20:40 +0700

    code-ai-ss3

--- src/main.ts
+++ src/main.ts
@@ -802,7 +802,7 @@ async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
     minPoint = 18;
   } else {
-    minPoint = 18;
+    minPoint = 16;
   }
 
   const siteQueue = siteQueues[siteName];
