8cf3fa2eb2c10db7fa027b897f4941289b057a4d
Choochoth <choochoth@gmail.com>
2026-08-24 01:39:41 +0700

    okcode-ai-s4

--- src/types/jsbn.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "jsbn" {
-  export class BigInteger {
-    constructor(a: string | number | BigInteger, b?: number);
-    equals(a: BigInteger): boolean;
-    toString(): string;
-  }
-}
--- src/types/xmldom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'xmldom' {
-    export class DOMParser {
-      parseFromString(xml: string, mimeType: string): Document;
-    }
-  }
-  
