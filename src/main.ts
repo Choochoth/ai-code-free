ea1294518f5389df298002cd03271c64d9d62756
okcodeaugs-sys <okcodeaugs@gmail.com>
2026-08-27 06:55:25 +0700

    Update Point

--- src/main.ts
+++ src/main.ts
@@ -801,9 +801,9 @@ async function initializeService() {
 // 🚀 startProCodeLoop (รองรับ abort)
 async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
-    minPoint = 14;
+    minPoint = 15;
   } else {
-    minPoint = 14;
+    minPoint = 15;
   }
 
   const siteQueue = siteQueues[siteName];
