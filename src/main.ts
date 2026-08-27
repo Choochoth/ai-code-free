fc4d184e201273f6663ea97358e3aabb1d24c231
okcodeaugs-sys <okcodeaugs@gmail.com>
2026-08-27 07:00:19 +0700

    Update Point

--- src/main.ts
+++ src/main.ts
@@ -802,7 +802,7 @@ async function startProCodeLoop(siteName: string) {
   if (siteName == "thai_jun88k36") {
     minPoint = 15;
   } else {
-    minPoint = 18;
+    minPoint = 15;
   }
 
   const siteQueue = siteQueues[siteName];
