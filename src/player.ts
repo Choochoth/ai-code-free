4b865ceaf3222a7a607bbe7eb16da417ff76bcbc
okcodeaugs-sys <okcodeaugs@gmail.com>
2026-08-28 10:04:57 +0700

    Update players

--- src/player.ts
+++ src/player.ts
@@ -14,10 +14,10 @@ const playerPools: Record<string, PlayerPool> = {
   },
   thai_jun88k36: {
     very_high: ["nuschai", "aroon11", "nus9331", "ary11", "bank0760", "manus9331"],
-    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "manus9331"],
-    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "twich234", "manus9331"],
-    low: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234"],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234"]
+    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "nus3206", "manus9331"],
+    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "twich234", "manus9331", "nus3206"],
+    low: ["manus9331", "nus3206", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234"],
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "nus3206", "ary11", "twich234"]
   }
 } 
 
