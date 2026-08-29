e7d294f7fb2bde2c01f2def5322c98c38707e3b4
Choochoth <choochoth@gmail.com>
2026-08-29 21:18:10 +0700

    s01-deploy

--- src/player.ts
+++ src/player.ts
@@ -4,20 +4,21 @@ import path from "path";
 import { AppliedPlayer, ApplyCodeToday, PlayerPool, PlayerLock } from "./types/player";
 import { isPlayerBlocked , cleanupExpiredBlocks} from "./playerTracker";
 
+
 const playerPools: Record<string, PlayerPool> = {
   thai_789bet: {
-    very_high: ["aroon11", "nus9331", "nus3206", "areeroon", "manuchai", "manus9331", "sjd9331"],
-    high:  ["nus9331", "aroon11", "sjd9331", "areeroon", "manuchai", "manus9331", "nus3206"],
+    very_high: ["aroon11", "nus9331", "nus3206", "areeroon", "manus9331", "sjd9331"],
+    high:  ["nus9331", "aroon11", "sjd9331", "areeroon", "manus9331", "nus3206"],
     mid: ["aroon11", "nus3206", "areeroon", "nus9331", "sjd9331", "manus9331", "manuchai"],
     low: ["nus3206", "nus3206", "manus9331", "aroon11", "manuchai", "nus9331", "areeroon", "sjd9331"],
     all: ["poypy789", "manuchai", "nus3206", "manus9331", "nus9331", "aroon11", "areeroon", "sjd9331"]
   },
   thai_jun88k36: {
-    very_high: ["nuschai", "aroon11", "nus9331", "ary11", "bank0760", "manus9331"],
-    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "nus3206", "manus9331"],
-    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "twich234", "sjd9331", "aroon2511", "areeroon", "manus9331", "nus3206"],
-    low: ["manus9331", "nus3206", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234", "sjd9331", "aroon2511", "areeroon"],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "sjd9331", "aroon2511", "areeroon", "nus3206", "ary11", "twich234"]
+    very_high: ["nuschai", "aroon11", "nus9331", "ary11", "bank0760", "manus9331", "aroon2511"],
+    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "nus3206", "manus9331", "aroon2511"],
+    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "twich234", "sjd9331", "aroon2511", "manus9331", "nus3206"],
+    low: ["manus9331", "nus3206", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234", "sjd9331", "aroon2511"],
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "sjd9331", "areeroon", "aroon2511", "nus3206", "ary11", "twich234"]
   }
 } 
 
