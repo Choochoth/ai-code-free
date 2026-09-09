f2fc00fe87d2565128e0e950e4e4c530a85968b1
okcodeaugs-sys <okcodeaugs@gmail.com>
2026-09-10 01:48:17 +0700

    add players

--- src/player.ts
+++ src/player.ts
@@ -16,9 +16,9 @@ const playerPools: Record<string, PlayerPool> = {
   thai_jun88k36: {
     very_high: ["nuschai", "aroon11", "nus9331", "ary11", "bank0760", "manus9331", "nus3206", "twich234"],
     high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "nus3206", "manus9331", "aroon2511", "twich234"],
-    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "twich234", "aroon2511", "manus9331", "nus3206"],
-    low: ["manus9331", "nus3206", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234", "aroon2511"],
-    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "aroon2511", "nus3206", "ary11", "twich234"]
+    mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "twich234", "aroon2511", "manus9331", "nus3206", "mn3206", "manusvip", "manusj88", "manuchai"],
+    low: ["manus9331", "nus3206", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234", "aroon2511", "mn3206", "manusvip", "manusj88", "manuchai"],
+    all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "aroon2511", "nus3206", "ary11", "twich234", "mn3206", "manusvip", "manusj88", "manuchai"]
   }
 } 
 
