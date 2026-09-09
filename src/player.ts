00aaa6897b9ca0062a8ff201ff057d355d2b1dcd
okcodeaugs-sys <okcodeaugs@gmail.com>
2026-09-10 01:22:06 +0700

    Update player twitch

--- src/player.ts
+++ src/player.ts
@@ -14,8 +14,8 @@ const playerPools: Record<string, PlayerPool> = {
     all: ["poypy789", "manuchai", "nus3206", "manus9331", "nus9331", "aroon11", "areeroon", "sjd9331"]
   },
   thai_jun88k36: {
-    very_high: ["nuschai", "aroon11", "nus9331", "ary11", "bank0760", "manus9331", "nus3206"],
-    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "nus3206", "manus9331", "aroon2511"],
+    very_high: ["nuschai", "aroon11", "nus9331", "ary11", "bank0760", "manus9331", "nus3206", "twich234"],
+    high: ["nus9331", "bank0760", "ary11", "nuschai", "aroon11", "nus3206", "manus9331", "aroon2511", "twich234"],
     mid:  ["nus9331", "ary11", "bank0760", "aroon11", "nuschai", "twich234", "aroon2511", "manus9331", "nus3206"],
     low: ["manus9331", "nus3206", "nus9331", "aroon11", "nuschai", "bank0760", "ary11", "twich234", "aroon2511"],
     all: ["manus9331", "nus9331", "aroon11", "nuschai", "bank0760", "aroon2511", "nus3206", "ary11", "twich234"]
